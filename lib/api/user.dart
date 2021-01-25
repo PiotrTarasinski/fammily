5f6709a12c9b97b40627cbda5d9bcab21ac924c6
PiotrTarasinski <piotrt337@gmail.com>
2021-01-25 23:47:05 +0100

    localitzation features

--- lib/api/user.dart
+++ lib/api/user.dart
@@ -1,14 +1,17 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:geolocator/geolocator.dart';
 
 class FirestoreUserController {
-  static Future<void> addUser(String uid, String name) { // Add new user to firestore
+  static Future<void> addUser(String uid, String name) async { // Add new user to firestore
     CollectionReference users = FirebaseFirestore.instance.collection('users');
     // Call the user's CollectionReference to add a new user
-    return users
-        .add({'uid': uid, 'name': name, 'location': GeoPoint(0,0)})
+    return await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high).then((Position position) =>
+      users
+        .add({'uid': uid, 'name': name, 'location': GeoPoint(position.latitude, position.longitude)})
         .then((value) => print("User Added"))
-        .catchError((error) => print("Failed to add user: $error"));
+        .catchError((error) => print("Failed to add user: $error"))
+    );
   }
 
   static Future<QuerySnapshot> getUser(String uid) { // Get user instance from firestore with given id
